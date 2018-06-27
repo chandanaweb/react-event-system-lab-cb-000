7013b2497d1fd849bbdb339f4b3634f46680b048
chandana M <chandu11288@gmail.com>
2018-06-27 02:33:43 +0000

    automatically backed up by learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component{
+  pwd = () => {
+    console.log('Entering password...');
+  }
+  render(){
+    return(
+      <input type="password" keyUp={this.pwd}/>
+    )
+  }
+}
+
+export default EyesOnMe;
